1464e23078914b38753e33bd6bcd21d149935436
Brucknem <mbruckner94@gmail.com>
2018-04-07 21:41:35 +0200

    Switch to connect 3

--- app/src/main/java/com/marcelbruckner/chapter4/MainActivity.java
+++ app/src/main/java/com/marcelbruckner/chapter4/MainActivity.java
@@ -1,7 +1,10 @@
 package com.marcelbruckner.chapter4;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fade(View view){
+        ImageView bart = (ImageView) view;
+        bart.animate().alpha(0f).setDuration(2000);
+
+        ImageView homer = findViewById(R.id.homer);
+        homer.animate().alpha(1f).setDuration(2000);
     }
 }
