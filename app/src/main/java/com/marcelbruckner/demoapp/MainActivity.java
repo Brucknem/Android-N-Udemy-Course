9aaf77003ca36567aa898b16c02969c0a7370a25
Brucknem <mbruckner94@gmail.com>
2018-04-07 20:53:18 +0200

    Finished

--- app/src/main/java/com/marcelbruckner/demoapp/MainActivity.java
+++ app/src/main/java/com/marcelbruckner/demoapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.marcelbruckner.demoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
